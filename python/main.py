903d023f5b747accc195e527836e530810d5d432
gregorkschroeder <gregorkschroeder@gmail.com>
2021-04-25 12:11:25 -0700

    initial 2019 INRIX data-sets commit

--- python/main.py
+++ python/main.py
@@ -1,92 +1,191 @@
+import geopandas as gpd
 import os
 import pandas as pd
-import settings  # import user created settings file
+import pyodbc
+import shutil
 from zipfile import ZipFile
 
 
-# load the INRIX TMC geometry file to get the TMC WKT geometry ----
-# the year of the TMC network is currently unknown and was
-# created in 2015 with limited documentation
-tmcGeo = pd.read_csv("../data/tmcGeoPre2016.csv")
+# set SQL instance and database containing INRIX objects
+server = ""  # TODO: set SQL instance
+db = ""  # TODO: set SQL database
+
+# set path to INRIX datasets
+inrixPath = ""  # TODO: set path to INRIX datasets
+
+# create SQL connection string to database
+connStr = "DRIVER={ODBC Driver 17 for SQL Server};" + \
+          "SERVER=" + server + ";" + \
+          "DATABASE=" + db + ";" + \
+          "Trusted_Connection=yes;"
+
+conn = pyodbc.connect(connStr)
 
 
-# load all zip files in the data folder ----
-# into their respective PeMS SQL object
-for root, dirs, files in os.walk(settings.folder):
+# load the geography zip file in the data folder -----------------------------
+xd_geo = gpd.read_file("zip://../data/USA_California.zip")
+
+# restrict to San Diego and surrounding counties
+xd_geo = xd_geo[xd_geo["County"].isin(
+    ["San Diego",
+     "Imperial",
+     "Riverside",
+     "Orange"]
+)]
+
+xd_geo = xd_geo.to_crs("EPSG:2230")  # set to SANDAG crs
+xd_geo["shape"] = [geo.wkt for geo in xd_geo.geometry]  # append WKT geometry
+xd_geo["vintage"] = "2019 Datasets"  # hardcoded dataset vintage
+
+xd_df = xd_geo[[
+    "vintage",
+    "XDSegID",
+    "FRC",
+    "RoadNumber",
+    "RoadName",
+    "County",
+    "District",
+    "Miles",
+    "Lanes",
+    "SlipRoad",
+    "SpecialRoa",
+    "RoadList",
+    "Bearing",
+    "XDGroup",
+    "shape"
+]]
+
+
+# load geography to SQL server -----------------------------------------------
+# write xd segment metadata to temporary csv file
+# note this csv file must be in a location visible to SQL server
+metadataPath = "../data/xdseg_metadata.csv"
+xd_df.to_csv(metadataPath, index=False, sep="|")
+
+# bulk insert xd segment metadata to SQL Server temporary table
+# then insert to final SQL table transforming WKT to geometry
+with conn.cursor() as cursor:
+    sqlTT = "DROP TABLE IF EXISTS [inrix].[temp_xdseg_metadata];" \
+            "CREATE TABLE [inrix].[temp_xdseg_metadata] (" \
+            "[vintage] nvarchar(20) NOT NULL," \
+            "[xdsegid] [int] NOT NULL," \
+            "[frc] [smallint] NOT NULL," \
+            "[road_number] [smallint] NULL," \
+            "[road_name] [nvarchar](150) NULL," \
+            "[county] [nvarchar](10) NOT NULL," \
+            "[district] [nvarchar](25) NULL," \
+            "[miles] [float] NOT NULL," \
+            "[lanes] [float] NOT NULL," \
+            "[slip_road] [bit] NOT NULL," \
+            "[special_road] [nvarchar](5) NULL," \
+            "[road_list] [nvarchar](100) NULL," \
+            "[bearing] [nvarchar](5) NOT NULL," \
+            "[xdgroup] [int] NOT NULL," \
+            "[shape] [nvarchar](max) NOT NULL," \
+            "CONSTRAINT [pk_inrix_temp_xdseg_metadata] PRIMARY KEY ([vintage], [xdsegid]))"
+
+    cursor.execute(sqlTT)
+    cursor.commit()
+
+    sqlBI = "BULK INSERT [inrix].[temp_xdseg_metadata] FROM '" + \
+            os.path.realpath(metadataPath) + "' " + \
+            "WITH (FIRSTROW = 2, TABLOCK, CODEPAGE = 'ACP', " + \
+            "FIELDTERMINATOR='|', ROWTERMINATOR='0x0a', MAXERRORS=0);"
+
+    cursor.execute(sqlBI)
+    cursor.commit()
+
+    sqlInsert = "INSERT INTO [inrix].[xdseg_metadata] (" \
+                "[vintage], [xdsegid], [frc], [road_number]," \
+                "[road_name], [county], [district]," \
+                "[miles], [lanes], [slip_road], [special_road], [road_list]," \
+                "[bearing], [xdgroup], [shape]) " \
+                "SELECT [vintage], [xdsegid], [frc], [road_number]," \
+                "[road_name], [county], [district]," \
+                "[miles], [lanes], [slip_road], [special_road], [road_list]," \
+                "[bearing], [xdgroup]," \
+                "geometry::STGeomFromText([shape], 2230).MakeValid()" \
+                "FROM [inrix].[temp_xdseg_metadata]; " \
+                "DROP TABLE [inrix].[temp_xdseg_metadata]"
+    cursor.execute(sqlInsert)
+    cursor.commit()
+
+os.remove(metadataPath)
+
+
+# load INRIX speed data ------------------------------------------------------
+# store xd segment metadata surrogate key lookup
+xdQry = "SELECT [segment_id], [xdsegid] " \
+        "FROM [inrix].[xdseg_metadata]" \
+        "WHERE [vintage] = '2019 Datasets'"
+xdLookup = pd.read_sql_query(xdQry, conn)
+
+for root, dirs, files in os.walk(inrixPath):
     # for each file in the data folder
     for file in files:
-        print("Loading: " + file)
         path = os.path.join(root, file)
 
         # for each file in the data folder
         if not file.endswith(".zip"):
             pass  # ignore non-zip files
         else:
-            with ZipFile(path) as zipfile:  # within the zip archive
-                # extract speed data to data folder
-                speedCsv = file.rstrip(".zip") + ".csv"
-                speedPath = os.path.join(root, speedCsv)
-                zipfile.extract(member=speedCsv, path=root)
+            with ZipFile(path) as zipData:  # within the zip archive
+                print("Loading: " + file)
 
-                # bulk insert speed data to SQL Server
-                with settings.conn.cursor() as cursor:
-                    sql = "BULK INSERT [inrix].[speed_data_pre2017] FROM '" + \
-                          os.path.realpath(speedPath) + "' " + \
-                          "WITH (FIRSTROW = 2, TABLOCK, CODEPAGE = 'ACP', " + \
-                          "FIELDTERMINATOR=',', ROWTERMINATOR='0x0a');"
-                    cursor.execute(sql)
-                    cursor.commit()
+                # extract data csv file from archive
+                speedPath = file.rstrip(".zip") + "/data.csv"
+                zipData.extract(member=speedPath, path=root)
 
-                # remove extracted speed data
-                os.remove(speedPath)
+            # create SQL temporary table and bulk insert data csv file
+            with conn.cursor() as cursor:
+                sqlTT = "CREATE TABLE [inrix].[temp_speed_15min] (" \
+                        "[Date Time] datetimeoffset(0) NOT NULL," \
+                        "[Segment ID] bigint NOT NULL," \
+                        "[UTC Date Time] datetimeoffset(0) NOT NULL," \
+                        "[Speed(km/hour)] float NULL," \
+                        "[Hist Av Speed(km/hour)] float NULL," \
+                        "[Ref Speed(km/hour)] float NOT NULL," \
+                        "[Travel Time(Minutes)] float NULL," \
+                        "[CValue] float NULL," \
+                        "[Pct Score30] float NULL," \
+                        "[Pct Score20] float NULL," \
+                        "[Pct Score10] float NULL," \
+                        "[Road Closure] nvarchar(5) NULL," \
+                        "[Corridor/Region Name] nvarchar(40) NOT NULL," \
+                        "CONSTRAINT pk_inrix_temp_speed_15min PRIMARY KEY ([Date Time], [Segment ID]))"
+                cursor.execute(sqlTT)
+                cursor.commit()
 
-                print("Loaded: Speed data-set")
+                sqlBI = "BULK INSERT [inrix].[temp_speed_15min] FROM '" + \
+                        os.path.realpath(os.path.join(root, speedPath)) + "' " + \
+                        "WITH (FIRSTROW = 2, TABLOCK, CODEPAGE = 'ACP', " + \
+                        "FIELDTERMINATOR=',', ROWTERMINATOR='0x0a', MAXERRORS=0);"
+                cursor.execute(sqlBI)
+                cursor.commit()
 
-                # for the TMC metadata
-                with zipfile.open("TMC_Identification.csv") as tmcData:
-                    # read in TMC metadata as a pandas DataFrame
-                    df = pd.read_csv(tmcData)
+                # insert temporary table data into final SQL table
+                # applying transformations as necessary
+                # convert kmph to mph and append xd segment surrogate key
+                # note the dataset version is hardcoded in this query
+                # then drop the temporary table
+                sqlInsert = "INSERT INTO [inrix].[speed_15min] (" + \
+                            "[segment_id], [date_time], [speed]," + \
+                            "[hist_av_speed], [ref_speed], [travel_time]," + \
+                            "[c_value], [pct_score30], [pct_score20]," + \
+                            "[pct_score10], [road_closure]) " + \
+                            "SELECT [xdseg_metadata].[segment_id], [Date Time]," + \
+                            "[Speed(km/hour)] * 0.621371," + \
+                            "[Hist Av Speed(km/hour)] * 0.621371," + \
+                            "[Ref Speed(km/hour)] * 0.621371," + \
+                            "[Travel Time(Minutes)], [CValue], [Pct Score30]," + \
+                            "[Pct Score20], [Pct Score10], [Road Closure] " + \
+                            "FROM [inrix].[temp_speed_15min] " + \
+                            "INNER JOIN [inrix].[xdseg_metadata] " + \
+                            "ON [xdseg_metadata].[vintage] = '2019 Datasets' " + \
+                            "AND [temp_speed_15min].[Segment ID] = [xdseg_metadata].[xdsegid];" + \
+                            "DROP TABLE [inrix].[temp_speed_15min]"
+                cursor.execute(sqlInsert)
+                cursor.commit()
 
-                    # add date column indicating metadata reference date
-                    # zip archive naming convention is year_month_<<weekday/weekend>>.zip
-                    df["metadata_date"] = pd.to_datetime(file[0:4] + "-" + file[5:7] + "-01")
-
-                    # merge TMC geometry shape file with metadata table
-                    df = df.merge(right=tmcGeo,
-                                  how="left",
-                                  left_on="tmc",
-                                  right_on="tmc_code")
-
-                    # load pandas DataFrame to temporary table
-                    df.to_sql(name="tempMetadata",
-                              schema="inrix",
-                              con=settings.engine,
-                              if_exists="replace",
-                              index=False)
-
-                    # insert to TMC metadata table transforming WKT to geometry
-                    # and drop the temporary table
-                    with settings.conn.cursor() as cursor:
-                        sql = "INSERT INTO [inrix].[tmc_metadata_pre2017] " \
-                              "SELECT [metadata_date],[tmc],[road],[direction]," \
-                              "[intersection],[state],[county],[zip]," \
-                              "[miles],[road_order]," \
-                              "geometry::STGeomFromText([shape], 2230).MakeValid() " \
-                              "FROM [inrix].[tempMetadata]; " \
-                              "DROP TABLE [inrix].[tempMetadata]"
-                        cursor.execute(sql)
-                        cursor.commit()
-
-                    print("Loaded: TMC Metadata")
-
-
-# load the manually created cross-reference between the pre-2016 ----
-# INRIX TMC geometry and SANDAG's 2016 highway coverage
-# note this assumes the SQL instance can see this projects location
-with settings.conn.cursor() as cursor:
-    sql = "BULK INSERT [inrix].[highway_coverage_xref_pre2017] FROM '" + \
-          os.path.realpath("../data/tmcHwyCovXRef2015.csv") + "' " + \
-          "WITH (FIRSTROW = 2, TABLOCK, CODEPAGE = 'ACP', " + \
-          "FIELDTERMINATOR=',', ROWTERMINATOR='0x0a');"
-    cursor.execute(sql)
-    cursor.commit()
+            # remove extracted data csv file
+            shutil.rmtree(os.path.join(root, file.rstrip(".zip")))
